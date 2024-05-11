dc56b6cec40e13d66340c8f180ee49cb6dd51f81
siamak.mahmoudi <siamak.mahmoudi@okg.com>
2024-05-11 16:24:00 +0800

    Initial Commit

---
+++ Simple Calc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simple Calc
+//
+//  Created by Siamak Mahmoudi on 11/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simple Calc/Simple_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CalcApp.swift
+//  Simple Calc
+//
+//  Created by Siamak Mahmoudi on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Simple_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
